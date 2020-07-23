ac418c21f083fc063d74d61cbc39d35cb8566129
Emil Olsen <emilberolsen@gmail.com>
2020-07-23 08:21:20 +0200

    basic layout is done

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="Header"></div>
+    <div class="Content">
+      <router-view />
     </div>
-    <router-view />
+    <div class="Footer"></div>
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
